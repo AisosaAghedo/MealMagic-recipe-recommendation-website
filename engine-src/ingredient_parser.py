3123663558705fa64a2ae939c53918d94840f931
Jamescog <jamescog72@gmail.com>
2023-04-08 09:41:43 +0300

    Refactor the function to make it more pythonic

--- engine-src/ingredient_parser.py
+++ engine-src/ingredient_parser.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 """ This module is responsible for cleaning up data to feed for modeling
 """
+import os
 import pandas as pd
 import ast
 import re
 import unidecode
 from nltk.stem import WordNetLemmatizer
-import os
 
+# Define the file paths
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                           os.pardir))
 RECIPE_LINKS = os.path.join(parent_dir, 'input', 'recipes.csv')
 PARSED_RECIPE = os.path.join(parent_dir, 'input', 'parsed_recipe.csv')
 
 
-def ingredient_parser(ingreds):
-    """ This function will be responsible to clean ingredient data
-        Ingredient data is most cruical data to train the model and it should be cleaned
-        from common words, stop words and it should be uniform
+def ingredient_parser(ingreds: str) -> str:
+    """
+    Clean the ingredient data to prepare it for modeling.
+
+    Args:
+    ingreds: string, the raw ingredient data
+
+    Returns:
+    A cleaned string of parsed ingredients.
     """
 
+    # Define the most common words and measures to remove
     most_common_words = [
         "'1", "'2", 'teaspoon', 'of', 'g', ',', 'cup', 'fresh', "'½",
         'tablespoons', "oil',", 'tablespoon', 'ground', '', 'and', 'or', "'4",
@@ -50,39 +57,51 @@ def ingredient_parser(ingreds):
         'milli', 'centi', 'deci', 'hecto', 'kilo'
     ]
 
-    ingred_list = []
+    # Lemmatize the most common words and measures
     lemmatizer = WordNetLemmatizer()
     most_common_words = [
         lemmatizer.lemmatize(word) for word in most_common_words
     ]
     measures = [lemmatizer.lemmatize(measure) for measure in measures]
+
+    # Parse the raw ingredient data
     ingreds = ast.literal_eval(ingreds)
+    ingred_list = []
     for i in ingreds:
+        # Split the ingredient string into individual words
         items = re.split(' |-', i)
+        # Remove non-alpha words
         items = [word for word in items if word.isalpha()]
-        # Turn every word to lower case
+        # Convert all words to lowercase
         items = [word.lower() for word in items]
-        # remove accents
+        # Remove accents
         items = [unidecode.unidecode(word) for word in items]
-        # lemmatize the words
+        # Lemmatize the words
         items = [lemmatizer.lemmatize(word) for word in items]
-        # remove most common words
+        # Remove the most common words
         items = [word for word in items if word not in most_common_words]
-        # remove measuring words
+        # Remove the measures
         items = [word for word in items if word not in measures]
+        # Append the cleaned ingredient string to the list
         if items:
             ingred_list.append(' '.join(items))
 
+    # Join the cleaned ingredient strings into a single string
     return " ".join(ingred_list)
 
 
 if __name__ == "__main__":
+    # Read the recipe data into a pandas DataFrame
     df = pd.read_csv(RECIPE_LINKS)
+    # Apply the ingredient_parser function to the Recipe_ingredients column
     df['parsed_ingredients'] = df['Recipe_ingredients'].apply(
-        lambda x: ingredient_parser(x))
+        ingredient_parser)
+    # Select the desired columns
     new_df = df[[
         'Recipe_link', 'Recipe_name', 'Recipe_img', 'Recipe_ingredients',
         'parsed_ingredients', 'Recipe_directions'
     ]]
-    new_df.dropna()
-    df.to_csv(PARSED_RECIPE, index=False)
+    # Drop any rows with null values
+    new_df = new_df.dropna()
+    # Write the cleaned DataFrame to a CSV file
+    new_df.to_csv(PARSED_RECIPE, index=False)
