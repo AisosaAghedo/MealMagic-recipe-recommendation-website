421b601706d77c1440348280e5e55d24338d9395
Jamescog <jamescog72@gmail.com>
2023-04-23 18:59:43 +0300

    CREATE databa population script to populate database

---
+++ populate_db.py
@@ -0,0 +1,32 @@
+import MySQLdb as mysqldb
+import pandas as pd
+
+recipe_df = pd.read_csv('./input/recipes.csv')
+db_name = "recipe_recommendation"
+host = "localhost"
+user = "team"
+password = "password"
+
+db = mysqldb.connect(host=host, user=user, passwd=password, db=db_name)
+cursor = db.cursor()
+
+recipe_data = [
+    {
+        'recipe_url': row['Recipe_link'],
+        'name': row['Recipe_name'],
+        'img_url': row['Recipe_img'],
+        'ingredients': row['Recipe_ingredients'],
+        'directions': row['Recipe_directions'],
+    }
+    for index, row in recipe_df.iterrows()
+]
+
+for data in recipe_data:
+    query = "INSERT INTO recipes (name, img_url, ingredients, directions, recipe_url) VALUES (%s, %s, %s, %s, %s)"
+    values = [(data['name'], data['img_url'], data['ingredients'], data['directions'], data['recipe_url'])]
+    cursor.executemany(query, values)
+    db.commit()
+    print(f"{data['name']} is inserted successfully!")
+    
+print("Data inserted successfully")
+db.close()
