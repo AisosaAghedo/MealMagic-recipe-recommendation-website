76290e8cf47cabbe04a6c7a962941f59a0a6d421
Aisosa <allysonaghedo@gmail.com>
2023-04-26 17:12:27 +0100

    Creating a logged out home page

--- Frontend/MealMagic/src/Home/homeDesign.jsx
+++ Frontend/MealMagic/src/Home/homeDesign.jsx
@@ -3,6 +3,7 @@ import "./home.css";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAuth, logout } from "../signin/Signin";
+import ingredientsImg from '../assets/ingredients_img.jpg'
 
 const Loggedin_Home = ()=>{
   const navigate = useNavigate();
@@ -17,34 +18,36 @@ const Loggedin_Home = ()=>{
   };
 
   return (
-    <div className="home-container">
-      <div className="ingredients">
-        <label htmlFor="Ingrediets" className="label">
-          <h2>please enter list of ingredients available</h2>
-        </label>
-        <input
-          className="home_input
+    <div className="layout">
+      <div className="home-container">
+        <div className="ingredients">
+          <label htmlFor="Ingrediets" className="label">
+            <h2>please enter list of ingredients available</h2>
+          </label>
+          <input
+            className="home_input
         "
-          type="text"
-          placeholder="Ingredients"
-          {...register("ingredients", { required: true })}
-        />
-        {errors.ingredients && (
-          <p style={{ color: "red" }}>
-            <small>Ingredients are required</small>
-          </p>
-        )}
-        <div className="available">
-          pepper, salt and other common ingredients are assummed to be
-          available.
+            type="text"
+            placeholder="Ingredients"
+            {...register("ingredients", { required: true })}
+          />
+          {errors.ingredients && (
+            <p style={{ color: "red" }}>
+              <small>Ingredients are required</small>
+            </p>
+          )}
+          <div className="available">
+            pepper, salt and other common ingredients are assummed to be
+            available.
+          </div>
+          <button
+            type="submit"
+            className="home-btn"
+            onClick={handleSubmit(submitIngredients)}
+          >
+            Get Recommendation
+          </button>
         </div>
-        <button
-          type="submit"
-          className="home-btn"
-          onClick={handleSubmit(submitIngredients)}
-        >
-          Get Recommendation
-        </button>
       </div>
     </div>
   );
@@ -53,10 +56,28 @@ const Loggedin_Home = ()=>{
 
 const LoggedOut_Home = ()=>{
   return (
-    <div>
-      HEY
+    <div className="loggedout">
+      <div class="content">
+        <div class="text">
+          <h1>
+            MealMagic
+          </h1>
+          <p>
+            Pepsi is a carbonated soft drink manufactured by PepsiCo. <br />
+            Originally created and developed in 1893 by Caleb Bradham <br />
+            and introduced as Brad's Drink, it was renamed as Pepsi-Cola in
+            1898, and then shortened to Pepsi in 1961.
+          </p>
+        </div>
+        <div class="pepsi">
+          <img src={ingredientsImg} alt="image" />
+        </div>
+      </div>
+      <div className="button">
+        <button class="btn3">Buy Now</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default function HomeDesign() {
