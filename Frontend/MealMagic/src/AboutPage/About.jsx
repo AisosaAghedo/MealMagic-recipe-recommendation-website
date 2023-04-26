e7288b2f642f459442c9fbeede706f639da83e9a
horlamyday <horlamyday007@gmail.com>
2023-04-26 17:16:25 +0100

    my Aboupage

---
+++ Frontend/MealMagic/src/AboutPage/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Navbar from "../navbar/navbar";
+import AboutUs from "./AboutUs";
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <AboutUs /> 
+    </div>
+  )
+};
+export default About;
--- Frontend/MealMagic/src/app.jsx
+++ Frontend/MealMagic/src/app.jsx
@@ -10,6 +10,7 @@ import "./index.css";
 import Layout from "./layout/layout";
 import Login from "./signin/login";
 import Recipe from "./Recipepage/Recipe";
+import About from "./AboutPage/About";
 
 
 const router = createBrowserRouter(
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/Recipe/:ingredients" element={<Recipe />} />
+        <Route path="/About" element={<About/>} />
       </Route>
     </>
   )
